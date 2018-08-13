bd4707a34944f37969c99f7ddd799d06dad5f9ed
pavanpatil96 <pavanpatil196@gmail.com>
2018-08-13 23:35:12 +0530

    commiting code

---
+++ Selenium_Test/src/JavaforTesting/PrimitiveDataType.java
@@ -0,0 +1,20 @@
+package JavaforTesting;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		
+		int a=8;
+		long b=8;
+		double c= 8.5;
+		float d= 8;
+		
+		System.out.println(a+b+c+d);
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Selenium_Test/src/JavaforTesting/SimpleJavaCode.java
@@ -0,0 +1,10 @@
+package JavaforTesting;
+
+public class SimpleJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("CHEKING JAVA CONNECTION");
+	}
+
+}
